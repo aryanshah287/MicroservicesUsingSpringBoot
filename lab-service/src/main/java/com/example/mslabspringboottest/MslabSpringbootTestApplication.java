a6f364f9ce0e7017c2b2f505d4f4ccc60a7e25b9
aryanshah287 <99818273+aryanshah287@users.noreply.github.com>
2023-08-24 12:19:35 +0530

    committed

--- lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
+++ lab-service/src/main/java/com/example/mslabspringboottest/MslabSpringbootTestApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class MslabSpringbootTestApplication {
 
 	public static void main(String[] args) {
-		// SpringApplication.run(MslabSpringbootTestApplication.class, args);
 		System.out.println('Hello Zain!');
 		System.out.println('Hello Aryan!');
 		System.out.println('Hello Rijul!');
